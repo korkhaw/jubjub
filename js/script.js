e1d8c46e514114e3afe76c81c8f50a5426b95196
Lovelyii <korkhawroothamnong@gmail.com>
2026-02-14 08:48:41 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -87,24 +87,3 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
-
-const slider = document.getElementById("slider");
-const items = document.querySelectorAll(".gallery-item");
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
-
-let currentIndex = 0;
-
-function updateSlider() {
-  slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-nextBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % items.length; // loop
-  updateSlider();
-});
-
-prevBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex - 1 + items.length) % items.length; // loop
-  updateSlider();
-});
